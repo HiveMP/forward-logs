620023f881f22a0336a2e14c18874bfd58e6557d
June Rhodes <jrhodes@redpoint.games>
2018-11-04 05:27:50 +1100

    Try to fix nexe crash still by setting enableStdIn to false

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require("gulp");
 const mkdirp = require("mkdirp");
+const { compile } = require("nexe");
 
 gulp.task("default", async () => {
   mkdirp.sync("dist");
+
+  compile({
+    input: "./index.js",
+    output: "dist/forward-logs",
+    enableStdIn: false
+  }).then(() => {
+    console.log("success");
+  });
 });
