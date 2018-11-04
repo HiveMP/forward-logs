4e48a5249dddbd44e2adc3061e6e7f165bf79650
June Rhodes <jrhodes@redpoint.games>
2018-11-04 15:32:13 +1100

    Update nexe workaround for non-Windows OS

--- index.ts
+++ index.ts
@@ -13,23 +13,34 @@ module.constructor.prototype.require = function(path) {
   } catch (err) {
     if (path.indexOf("pty.node") !== -1) {
       // This is the pty.node file, extract assets then try again.
-      if (!existsSync("pty.node")) {
-        writeFileSync(
-          join(__dirname, "pty.node"),
-          readFileSync("./node_modules/node-pty/build/Release/pty.node")
-        );
-      }
-      if (!existsSync("winpty-agent.exe")) {
-        writeFileSync(
-          join(__dirname, "winpty-agent.exe"),
-          readFileSync("./node_modules/node-pty/build/Release/winpty-agent.exe")
-        );
-      }
-      if (!existsSync("winpty.dll")) {
-        writeFileSync(
-          join(__dirname, "winpty.dll"),
-          readFileSync("./node_modules/node-pty/build/Release/winpty.dll")
-        );
+      if (process.platform === "win32") {
+        if (!existsSync("pty.node")) {
+          writeFileSync(
+            join(__dirname, "pty.node"),
+            readFileSync("./node_modules/node-pty/build/Release/pty.node")
+          );
+        }
+        if (!existsSync("winpty-agent.exe")) {
+          writeFileSync(
+            join(__dirname, "winpty-agent.exe"),
+            readFileSync(
+              "./node_modules/node-pty/build/Release/winpty-agent.exe"
+            )
+          );
+        }
+        if (!existsSync("winpty.dll")) {
+          writeFileSync(
+            join(__dirname, "winpty.dll"),
+            readFileSync("./node_modules/node-pty/build/Release/winpty.dll")
+          );
+        }
+      } else {
+        if (!existsSync("pty.node")) {
+          writeFileSync(
+            join(__dirname, "pty.node"),
+            readFileSync("./node_modules/node-pty/build/Release/pty.node")
+          );
+        }
       }
       return self.constructor._load(join(__dirname, "pty.node"), self);
     } else {
